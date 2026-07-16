83accb76b9ba0eac4a7ec553ee66d73820e70781
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-16 23:47:48 +0500

    Date Validation task completed today

--- Java/Tasks/src/DateValidation.java
+++ Java/Tasks/src/DateValidation.java
@@ -6,17 +6,52 @@ public class DateValidation {
         int day;
         int month;
         int year;
+        boolean validDate = true;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter Day: ");
         day = scanner.nextInt();
         System.out.print("Enter Month: ");
         month = scanner.nextInt();
+        scanner.nextLine();
         System.out.print("Enter Year: ");
         year = scanner.nextInt();
 
-        if(day>=1 && day<=31)
-        //System.out.printf("Date : %d:%d:%d",day,month,year);
-
-
+        if(day<1 || day>31){
+            validDate = false;
+        } else if (month<1 || month >12) {
+            validDate = false;
+        } else if (year<1000 || year>9999) {
+            validDate = false;
+        }
+        else{
+            if(month == 2){
+                if(year%400==0 || (year %4== 0 && year%100 != 0)){
+                    if(day>29){
+                        validDate = false;
+                    }
+                }
+                else{
+                    if(day>28){
+                        validDate = false;
+                    }
+                }
+            }
+            else if (month == 4  || month == 6  || month == 9  || month == 11){
+                if(day>30){
+                    validDate = false;
+                }
+            }
+            else{
+                validDate = true;
+            }
+        }
+        System.out.println("-----------------------");
+        if(validDate){
+            System.out.printf("%d:%d:%s = VALID DATE",day,month,year);
+        }
+        else{
+            System.out.printf("%d:%d:%s = INVALID DATE",day,month,year);
+        }
+        scanner.close();
     }
 }
