481f59ed23cd7fb19544f465730c6c3db2cd432d
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-18 15:21:24 +0500

    Added Validation and next second time guessing complete logic. Task Completed.

--- Java/Tasks/src/timeValidationAndNextSecondTime.java
+++ Java/Tasks/src/timeValidationAndNextSecondTime.java
@@ -18,6 +18,40 @@ public class timeValidationAndNextSecondTime {
         System.out.print("Enter Seconds: ");
         seconds = scanner.nextInt();
 
-        System.out.printf("Time = %s:%s:%s",hours,minutes,seconds);
+        //System.out.printf("Time = %s:%s:%s",hours,minutes,seconds);
+        if(hours<0 || hours > 23){
+            validTime = false;
+        }
+        else if(minutes < 0 || minutes >59){
+            validTime = false;
+        }
+        else if(seconds < 0 || seconds >59){
+            validTime = false;
+        }
+        else{
+            if(hours==23 && minutes==59 && seconds==59){
+                hours =00;
+                minutes = 00;
+                seconds =00;
+            }
+            else if(seconds<59){
+                seconds++;
+            }
+            else if(minutes==59 && seconds==59){
+                hours++;
+                minutes=00;
+                seconds=00;
+            }
+            else{
+                minutes++;
+                seconds=00;
+            }
+        }
+        if(validTime) {
+            System.out.printf("New Time = %s:%s:%s\n", hours, minutes, seconds);
+        }
+        else{
+            System.out.println("INVALID time");
+        }
     }
 }
