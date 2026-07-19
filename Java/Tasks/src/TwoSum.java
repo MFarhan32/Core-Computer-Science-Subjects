d5d2273dbc186c697b24961daf19a48f52ae4883
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-19 23:43:17 +0500

    String initialized in Matching sum of two numbers with target Task

---
+++ Java/Tasks/src/TwoSum.java
@@ -0,0 +1,8 @@
+public class TwoSum {
+    public static void main(String[] args){
+        int[] numbers = {2,4,5,3,8};
+        for(int i=0;i<numbers.length;i++){
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
