ef3f4dfa3d9eb0fa9422e2bb2031c009afa8db04
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-19 23:56:35 +0500

    Improved loop logic for 'Two sum integers equal to target ' task

--- Java/Tasks/src/TwoSum.java
+++ Java/Tasks/src/TwoSum.java
@@ -9,9 +9,10 @@ public class TwoSum {
         System.out.print("Enter value of Target: ");
         target = scanner.nextInt();
         for(int i=0;i<numbers.length-1;i++){
-            for (int j=1;j<numbers.length;j++){
+            for (int j=i+1;j<numbers.length;j++){
                 if (numbers[i] + numbers[j]==target){
-                    System.out.println("The sum of "+ numbers[i]+ " and "+numbers[j]+" is " + target );
+                    //System.out.println("The sum of "+ numbers[i]+ " and "+numbers[j]+" is " + target );
+                    System.out.println("The sum of indices of "+ i+ " and "+j+" is " + target );
                     matchTarget = true;
                     break;
                 }
