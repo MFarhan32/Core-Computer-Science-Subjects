0c7fbb586d88a0ac1f282553776fae0649d774fa
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-20 00:38:58 +0500

    Logic improved in Stock Profit task

--- Java/Tasks/src/MaxProfit.java
+++ Java/Tasks/src/MaxProfit.java
@@ -1,26 +1,21 @@
-import java.sql.SQLOutput;
-
 public class MaxProfit {
+    static int maxProfit(int[] stock){
 
+        int min = stock[0];
+        int profit = 0;
+        for (int i=1;i<stock.length;i++){
 
-    static void checkProfit(int[] stock, int profit){
+            if (min>stock[i]){
+                min = stock[i];
+            }
 
-        for (int i=0;i<stock.length-1;i++){
-            for (int j=i+1;j<stock.length;j++){
-                int prof=stock[j]-stock[i];
-                if(prof>profit){
-                    profit= prof;
-                }
+            if(stock[i]-min>profit){
+                profit = stock[i]-min;
             }
         }
-        if (profit>0){
-            System.out.print("Profit is: "+ profit);
-        }
-        else{
-            System.out.print("NO Profit");
-        }
-    }
+        return profit;
 
+    }
 
     public static void main(String[] args){
         /*You are given an array where each element represents the stock price on that day.
@@ -28,9 +23,8 @@ public class MaxProfit {
         If no profit is possible, return 0.
         Note : first integer is buying price and after this price is selling price
         */
+        int[] stock ={7,1,5,3,6,4};
 
-        int[] stock ={7,4,5,3,6,7};
-        int profit =0;
-        checkProfit(stock,profit);
+        System.out.println(maxProfit(stock));
     }
 }
