468c1e6204399bbe82f14962273dc962b16fa990
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-21 23:40:37 +0500

    Runtime polymorphism added in polymorphism topic

---
+++ Java/Polymorphism/src/MainRunTime.java
@@ -0,0 +1,8 @@
+public class MainRunTime {
+    public static void main(String[] args){
+        //Vehicle vehicle = new Bike();
+        //Vehicle vehicle = new Boat();
+        Vehicle vehicle = new Car();
+        vehicle.go();
+    }
+}
