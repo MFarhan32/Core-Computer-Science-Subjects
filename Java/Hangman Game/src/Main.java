2d18f697b16140920ba459427b1d6e05871a0350
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-23 17:29:41 +0500

    Random word picked using random index

--- Java/Hangman Game/src/Main.java
+++ Java/Hangman Game/src/Main.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,10 +16,32 @@ public class Main {
         String filePath = "words.txt";
         BufferedReader br =new BufferedReader(new FileReader(filePath));
         String line;
-        while((line=br.readLine())!=null){
-            System.out.println(line.trim());
-        }
+        char guess;
+        int guessCount=0;
+        ArrayList<String> words = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
+        while((line=br.readLine())!=null){
+            words.add(line);
+            //System.out.println(line.trim());
+        }
+        Random random = new Random();
+        String word =words.get(random.nextInt(words.size()));
+        System.out.println("***********************");
+        System.out.println("Welcome to Hangman Game");
+        System.out.println("***********************");
+        for (int i=0;i<word.length();i++){
+            System.out.print(" _ ");
+        }
+        System.out.println();
+        System.out.print("Guess your Letter: ");
+        guess = sc.next().toUpperCase().charAt(0);
+
+//        for (int i=0; i<word.length();i++){
+//            if(guess==word.charAt(i)){
+//                word.replace()
+//            }
+//        }
         br.close();
     }
 }
