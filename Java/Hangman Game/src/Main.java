7fd5e42ce16010cc48ad8afd6d69070644c9d2e3
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-23 23:43:55 +0500

    Hangman game completed. (hangmanArt method also added.)

--- Java/Hangman Game/src/Main.java
+++ Java/Hangman Game/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         /*File Creation
         String filePath = "words.txt";
-        String content = "Banana\nOrange\n";
+        String content = "Banana\nOrange\nMango";
          File file = new File(filePath);
         FileWriter fw = new FileWriter(file);
         fw.write(content);
@@ -16,8 +16,7 @@ public class Main {
         String filePath = "words.txt";
 
         String line;
-        char guess;
-        int guessCount=0;
+
         ArrayList<String> words = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
@@ -34,18 +33,24 @@ public class Main {
             System.out.println("Something went wrong");
         }
 
+        int guessCount=0;
+
+
+        Random random = new Random();
+        String word =(words.get(random.nextInt(words.size()))).toLowerCase();
+        ArrayList<Character> wordState = new ArrayList<>();
+        System.out.println("***********************");
+        System.out.println("Welcome to Hangman Game");
+        System.out.println("***********************");
+        for (int i=0;i<word.length();i++){
+            wordState.add('_');
+        }
 
         while(guessCount<6){
-            Random random = new Random();
-            String word =words.get(random.nextInt(words.size()));
-            ArrayList<Character> wordState = new ArrayList<>();
-            System.out.println("***********************");
-            System.out.println("Welcome to Hangman Game");
-            System.out.println("***********************");
-            for (int i=0;i<word.length();i++){
-                wordState.add('_');
-            }
 
+            System.out.println(getHangmanArt(guessCount));
+            char guess;
+            System.out.print("Word: ");
             for (char ch : wordState){
                 System.out.print(ch);
             }
@@ -53,18 +58,64 @@ public class Main {
             System.out.print("Guess your Letter: ");
             guess = sc.next().toLowerCase().charAt(0);
 
-            for (int i=0; i<word.length();i++){
-                if(guess==word.charAt(i)){
-                    System.out.println("Correct guess.");
-                    wordState.set(i,guess);
+            if(word.indexOf(guess)>=0) {
+                System.out.println("Correct Guess 👏.");
+                for (int i = 0; i < word.length(); i++) {
+                    if (guess == word.charAt(i)) {
+                        wordState.set(i, guess);
+                    }
                 }
-                else{
-                    guessCount++;
-                    System.out.println("Incorrect Guess.");
+                if(!wordState.contains('_')){
+                    System.out.println(getHangmanArt(guessCount));
+                    System.out.println("Congratulations. You won 🎊.");
+                    break;
                 }
             }
-
+            else{
+                System.out.println("Wrong Guess 👎.");
+                guessCount++;
+            }
         }
+        if(guessCount>=6){
+            System.out.println(getHangmanArt(guessCount));
+            System.out.println("Game Over 😩.");
+            System.out.println("The word was : "+word);
+        }
+    }
+
+    static String getHangmanArt(int wrongGuesses){
+        return switch(wrongGuesses) {
+            case 0 -> """
+                    """;
+            case 1 -> """
+                     o
+                    """;
+            case 2 -> """
+                     o
+                     |
+                    """;
+            case 3 -> """
+                     o
+                    /|
+                    """;
+            case 4 -> """
+                     o
+                    /|\\
+                    """;
+            case 5 -> """
+                     o
+                    /|\\
+                    /
+                    """;
+            case 6 -> """
+                     o
+                    /|\\
+                    / \\
+                    """;
+            default -> "Invalid";
+
+        };
+
 
     }
 }
