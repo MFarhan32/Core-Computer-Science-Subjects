852b0248d281d4618c6e617049a1c235c68a9fad
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-28 00:04:18 +0500

    Dealing with both default and manual exceptions

--- Java/ManualException/src/Main.java
+++ Java/ManualException/src/Main.java
@@ -2,21 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class FarhanException extends Exception{
     FarhanException(String str){
-
+        super(str);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        int i = 20;
+        int i = 30;
         int j = 0;
+
         try {
             j = 18 / i;
-            if(i>j){
-            throw new FarhanException("Divided by zero exception.");}
-        } catch (ArithmeticException  | FarhanException e) {
-            System.out.println(e);
-            //System.out.println("Arithmetic exception.");
+            if(j==0){
+                throw new FarhanException("Divided by zero exception.");}
+        } catch (ArithmeticException e) {
+            System.out.println("default arithmetic exception and "+e.getMessage());
+        }
+        catch (FarhanException e){
+            System.out.println("Manual Exception and "+ e.getMessage());
         }
 
         System.out.println(j);
