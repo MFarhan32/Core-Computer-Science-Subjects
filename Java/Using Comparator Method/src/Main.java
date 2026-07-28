0c74d96cdc747a7688d26953c5c7500160c76126
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-28 22:33:27 +0500

    Adding 'Comparator method in lambda expression by Collections class in sorting student age

--- Java/Using Comparator Method/src/Main.java
+++ Java/Using Comparator Method/src/Main.java
@@ -9,12 +9,15 @@ public class Main {
         students.add(new Student("Mahnoor",5));
         students.add(new Student("Arslan",24));
 
-        Comparator<Student> com = new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                return (o1.age > o2.age) ? 1 : -1;
-            }
-        };
+//        Comparator<Student> com = new Comparator<Student>() {
+//            @Override
+//            public int compare(Student o1, Student o2) {
+//                return (o1.age > o2.age) ? 1 : -1;
+//            }
+//        };
+
+        Comparator <Student> com = (Student o1, Student o2) -> (o1.age > o2.age) ? 1 : -1;
+
 
         Collections.sort(students,com);
 
