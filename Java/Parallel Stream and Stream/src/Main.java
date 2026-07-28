e6d83a13c59d013e8202e97207d42849dc234572
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-07-29 00:02:37 +0500

    Stream and Parallel Stream time comparison

--- Java/Parallel Stream and Stream/src/Main.java
+++ Java/Parallel Stream and Stream/src/Main.java
@@ -9,16 +9,26 @@ public class Main {
 
         List<Integer> nums = Arrays.asList(2,5,3,6,7,4);
 
+        long startSeq = System.currentTimeMillis();
         int sum1 = nums.stream()
                 .map(i -> i*2)
                 .mapToInt(i -> i)
                 .sum();
+        long endSeq = System.currentTimeMillis();
+
+        long startPara = System.currentTimeMillis();
 
         int sum2 = nums.parallelStream()
                 .map(i -> i*2)
                 .mapToInt(i -> i)
                 .sum();
 
+        long endPara= System.currentTimeMillis();
+
         System.out.println(sum1  + " " + sum2);
+        System.out.print("Stream Time : ");
+        System.out.println(endSeq - startSeq);
+        System.out.print("ParallelStream Time : ");
+        System.out.println(endPara - startPara);
     }
 }
